58e055c435dc55d9283f5b787338a14618f7602a
Re4son <Re4son@users.noreply.github.com>
2017-01-27 20:51:39 +1100

    Fix shutdown

--- Menus/menu-1.py
+++ Menus/menu-1.py
@@ -108,7 +108,7 @@ def button(number):
 
         # Shutdown
         pygame.quit()
-        run_cmd("/usr/bin/sudo /sbin/shutdown -r now")
+        run_cmd("/usr/bin/sudo /sbin/shutdown -h now")
         sys.exit()
 
     if number == 5:
